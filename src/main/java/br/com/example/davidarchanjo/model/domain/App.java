f1eb8d971f9a22446a8da54af975ef795d0b2e86
Amit-Agarwal1996 <anand.ag23@gmail.com>
2024-12-24 01:27:18 +0530

    Adding ast parser file for each method

--- src/main/java/br/com/example/davidarchanjo/model/domain/App.java
+++ src/main/java/br/com/example/davidarchanjo/model/domain/App.java
@@ -29,5 +29,89 @@ public class App {
     this.name = name;
     this.version = version;
   }
+ 
+  public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+// Getter and Setter for author
+public String getAuthor() {
+    return author;
+}
+
+public void setAuthor(String author) {
+    this.author = author;
+}
+
+// Getter and Setter for name
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+// Getter and Setter for version
+public String getVersion() {
+    return version;
+}
+
+public void setVersion(String version) {
+    this.version = version;
+}
+
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    App other = (App) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (author == null) {
+      if (other.author != null)
+        return false;
+    } else if (!author.equals(other.author))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (version == null) {
+      if (other.version != null)
+        return false;
+    } else if (!version.equals(other.version))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((author == null) ? 0 : author.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((version == null) ? 0 : version.hashCode());
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "App [id=" + id + ", author=" + author + ", name=" + name + ", version=" + version + "]";
+  }
 
 }
--- src/main/java/br/com/example/davidarchanjo/model/dto/AppDTO.java
+++ src/main/java/br/com/example/davidarchanjo/model/dto/AppDTO.java
@@ -29,4 +29,47 @@ public class AppDTO {
         this.version = version;
         this.author = author;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AppDTO other = (AppDTO) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (version == null) {
+            if (other.version != null)
+                return false;
+        } else if (!version.equals(other.version))
+            return false;
+        if (author == null) {
+            if (other.author != null)
+                return false;
+        } else if (!author.equals(other.author))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        result = prime * result + ((author == null) ? 0 : author.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AppDTO [name=" + name + ", version=" + version + ", author=" + author + "]";
+    }
+    
 }
